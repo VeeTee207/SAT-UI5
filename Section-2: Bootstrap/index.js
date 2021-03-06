353d160fd956de2e9efa92a6eb848f3fab8eba74
VeeTee207 <57884607+VeeTee207@users.noreply.github.com>
2021-03-05 23:20:36 -0500

    Create index.js

---
+++ Section-2: Bootstrap/index.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/m/Text"
+], function (Text) {
+    "use strict";
+
+    // console.log('testing');
+    new Text({
+        text: "hello UI5 five!!!!!"
+    }).placeAt('content');
+});
